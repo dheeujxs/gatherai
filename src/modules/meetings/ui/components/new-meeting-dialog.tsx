64812e53df869cae35623e34e13445f588af33fa
Dheeraj <goswamidheeraj760gmail.com>
2025-06-26 12:57:12 +0530

    17:meetings form

---
+++ src/modules/meetings/ui/components/new-meeting-dialog.tsx
@@ -0,0 +1,27 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { MeetingForm } from "./meetings-form";
+import { useRouter } from "next/navigation";
+
+
+
+interface NewMeetingDilogProps{
+    open:boolean;
+    onOpenChange : (open:boolean) => void;
+}
+
+export const NewMeetingDialog=({open, onOpenChange}:NewMeetingDilogProps) => {
+    const router = useRouter()
+    return (
+        <ResponsiveDialog title="New Meeting" descrption="Create a new Meeting" open={open} onOpenChange={onOpenChange}>
+      <MeetingForm 
+      onSuccess={(id) => {
+        onOpenChange(false)
+        router.push(`/meetings/${id}`)
+      }}
+      onCancel={() => onOpenChange}
+      />
+
+
+        </ResponsiveDialog>
+    )
+}
