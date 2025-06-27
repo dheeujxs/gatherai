5b45c52592514f505a222f2a0495f73f2ab4d21f
Dheeraj <goswamidheeraj760gmail.com>
2025-06-27 10:29:09 +0530

    18: meetings data table

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,9 +1,9 @@
 import { db } from "@/db";
-import {  meetings } from "@/db/schema";
+import {  agents, meetings } from "@/db/schema";
 import {  createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 import { z } from "zod";
-import { and, count, desc, eq, getTableColumns ,ilike } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns ,ilike, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingUpdateSchema } from "../schemas";
@@ -89,9 +89,12 @@ if(!existingMeeting){
         const data = await db
         .select({
             ...getTableColumns(meetings),
+            agent: agents,
+            duration:sql<number>`EXTRACT(EPOCH FROM (ended_at-started_at))`.as("duration"),
 
         })
         .from(meetings)
+        .innerJoin(agents , eq(meetings.agentId ,agents.id))
         .where(
             and(
                 eq(meetings.userId, ctx.auth.user.id),
@@ -106,6 +109,7 @@ if(!existingMeeting){
         const [total] = await db
         .select({count: count()})
         .from(meetings)
+             .innerJoin(agents , eq(meetings.agentId ,agents.id))
         .where(
             and(
                  eq(meetings.userId, ctx.auth.user.id),
