21772a38d3fe3b823b7e70ebedd6c30b8ced8373
Dheeraj <goswamidheeraj760gmail.com>
2025-07-07 17:36:34 +0530

    26:transcript and chat

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,15 +1,17 @@
 import { db } from "@/db";
-import {  agents, meetings } from "@/db/schema";
+import {  agents, meetings, user } from "@/db/schema";
 import {  createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 import { z } from "zod";
-import { and, count, desc, eq, getTableColumns ,ilike, sql } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns ,ilike, inArray, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingUpdateSchema } from "../schemas";
-import { MeetingStatus } from "../types";
+import { MeetingStatus, StreamTransciptItem } from "../types";
 import { streamVideo } from "@/lib/stream-video";
 import { generateAvatarUri } from "@/lib/avatar";
+import JSONL from "jsonl-parse-stringify";
+import { streamChat } from "@/lib/stream-chat";
 
 
 
@@ -17,9 +19,107 @@ import { generateAvatarUri } from "@/lib/avatar";
 
 
 export const meetingsRouter = createTRPCRouter({
+    gnerateChatToken: protectedProcedure.mutation(async({ctx}) => {
+     const token = streamChat.createToken(ctx.auth.user.id)
+        await streamChat.upsertUser({
+            role: "admin",
+            id: ctx.auth.user.id
+        })
+        return token;
+    }),
+    getTranscipt: protectedProcedure
+    .input(z.object({id:z.string()}))
+    .query(async({input,ctx}) => {
+        const [existingMeeting] = await db
+        .select()
+        .from(meetings)
+        .where(
+            and(eq(meetings.id , input.id), eq(meetings.userId , ctx.auth.user.id))
+        );
+        if(!existingMeeting){
+            throw new TRPCError({
+                code:"NOT_FOUND",
+                message:"Meetin not found"
+            });
+        }
+        if(!existingMeeting.transcriptUrl){
+            return [];
+        }
+
+        const transcirpt  = await fetch(existingMeeting.transcriptUrl)
+        .then((res) => res.text())
+        .then((text) =>JSONL.parse<StreamTransciptItem>(text))
+        .catch(() => {
+            return  [];
+        })
+
+        const speakerIds = [
+            ...new Set(transcirpt.map((item) => item.speaker_id))
+        ];
+
+        const userSpeakers = await db 
+        .select()
+        .from(user)
+        .where(inArray(user.id, speakerIds))
+        .then((users) => 
+        users.map((user) => ({
+            ...user,
+            image:
+            user.image ??
+            generateAvatarUri({seed:user.name,variant:"initials"})
+        }))
+    );
+    const agentSpeakers = await db
+    .select()
+    .from(agents)
+    .where(inArray(agents.id, speakerIds))
+    .then((agents) => 
+        agents.map((agent) => ({
+        ...agent,
+        image:generateAvatarUri({
+            seed:agent.name,
+            variant:"botttsNeutral"
+        }),
+
+
+    }))
+    )
+    const speakers = [...userSpeakers, ...agentSpeakers]
+
+    const transcriptWithSpeakers = transcirpt.map((item) =>{
+        const speaker = speakers.find(
+            (speaker) => speaker.id === item.speaker_id
+        );
+        if(!speaker){
+            return {
+                ...item,
+                user:{
+                    name:"Unknown",
+                    image:generateAvatarUri({
+                        seed:"Unknown",
+                        variant:"initials"
+                    })
+                }
+            }
+
+           
+        }
+
+         return {
+                ...item,
+                user:{
+                    name:speaker.name,
+                    image:speaker.image,
+                }
+            }
+    })
+    return transcriptWithSpeakers
+       
+    }),
     generateToken:protectedProcedure.mutation(async({ctx}) => {
         await streamVideo.upsertUsers([
             {
+
                 id: ctx.auth.user.id,
                 name: ctx.auth.user.name,
                 role:"admin",
